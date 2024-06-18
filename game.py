e1b96d0cbf1341aa482e0242915799724c33a767
radev1924 <radev1924@abv.bg>
2024-06-18 14:14:33 +0300

    added pygame class for modularity

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod
 
 
+
 class Game(ABV):
     def __init__(self, map_w: int, map_h: int) -> None:
     
@@ -29,4 +30,18 @@ class Game(ABV):
     if chance < SPAWN_CHANCE and tile.name != "water"
         return deepCopy(choice_enemies)
 
+class Pygamemode(Game)
+    def __init__(self, map_w: int = 30, map_h: int = 15) -> None:
+        super().__init__(map_h, map_w)
 
+        pygame.init()
+
+        self.tile_size = 10
+        self.hud_height = 140
+        self.screen_width = self.tile_size * map_w * 2 + self.tile_size * 2
+        self.screen_height = self.tile_size * map_h * 2 + self.hud_height + self.tile_size
+
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        self.canvas = pygame.Surface((self.map_w * self.tile_size, self.map_h * self.tile_size)).convert
+        self.map_background = pygame.Surface(( self.screen_width, self.screen_height -  self.hud_height)).convert_alpha()
+        self.map_background.fill("brown")
