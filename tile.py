32a610c4a34d8ad64a101921b5f6352a88bc583e
radev1924 <radev1924@abv.bg>
2024-06-21 13:11:13 +0300

    implement and rewrite map tile logic

--- tile.py
+++ tile.py
@@ -1,34 +1,32 @@
-ANSI_RESET = "\033[0m"
-ANSI_YELLOW = "\033[33m"
-ANSI_GREEN = "\033[32m"
-ANSI_BLUE = "\033[34m"
-ANSI_RED = "\033[31m"
-ANSI_WHITE = "\033[97m"
-ANSI_MAGENTA = "\033[35m"
-ANSI_CYAN = "\033[36m"
+# Standard library imports
+import os
 
+# Third-party imports
+import pygame
 
-"""
-I simplified the logic of tile objects by deprecating the colored parameter.
-The color parameter can also be used for the same purpose.
-
-
-colored example: 
-tile = Tile("x", ANSI_RED)
-
-non-colored example:
-tile = Tile("x")
-
-"""
+# Local folder imports
+from color import Color as c
 
 
 class Tile:
-    def __init__(self, symbol: str, color: str | None = None):
-        self.symbol = f"{color}{symbol}{ANSI_RESET}" if color else symbol
+    def __init__(self, symbol: str, name: str, color: str = c.ANSI_RESET) -> None:
+        self.symbol = symbol
+        self.name = name
+        self.legend = f"{symbol} {name.upper()}"
+
+        self.colored_symbol = f"{color}{symbol}{c.ANSI_RESET}"
+        self.colored_name = f"{color}{name.upper()}{c.ANSI_RESET}"
+        self.colored_legend = f"{self.colored_symbol} {self.colored_name}"
+
+    def load_image(self):
+        self.image = pygame.image.load(os.path.join("images", f"{self.name}.png")).convert_alpha()
 
 
-plains = Tile(".", ANSI_YELLOW)
-forest = Tile("8", ANSI_GREEN)
-pines = Tile("Y", ANSI_GREEN)
-mountain = Tile("A", ANSI_WHITE)
-water = Tile("~", ANSI_CYAN)
+plains = Tile(".", "plains", c.ANSI_YELLOW)
+forest = Tile("8", "forest", c.ANSI_GREEN)
+pines = Tile("Y", "pines", c.ANSI_GREEN)
+mountain = Tile("A", "mountain")
+water = Tile("~", "water", c.ANSI_CYAN)
+player_marker = Tile("X", "player", c.ANSI_RED)
+empty = Tile(" ", "???")
+town = Tile("M", "town", c.ANSI_MAGENTA)
